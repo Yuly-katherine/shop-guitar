c03c2f6606cf9b207aad2bf848518ed9816c596e
yuly-katherine <yuli-k@hotmail.com>
2025-09-03 10:35:27 -0500

    add useReducer

---
+++ src/reducers/cart-reducer.ts
@@ -0,0 +1,45 @@
+import type { CartItem, Guitar } from "../types";
+import { db } from "../data/db"
+
+export type CartActions = 
+{ type: 'add-to-cart', payload: {item: Guitar}} |
+{ type: 'remove-from-cart', payload: {id: Guitar['id']}} |
+{ type: 'decrease-quantity', payload: {id: Guitar['id']}} |
+{ type: 'increase-quantity', payload: {id: Guitar['id']}}
+
+export type CartState = {
+    data : Guitar[],
+    cart: CartItem[]
+}
+
+export const  initialState: CartState = {
+    data: db,
+    cart: []
+}
+
+export const cartReducer = (
+    state: CartState = initialState,
+    action: CartActions
+) => {
+    if( action.type === 'add-to-cart'){
+        return {
+            ...state
+        }
+    }
+    if( action.type === 'remove-from-cart'){
+        return{
+            ...state
+        }
+    }
+    if( action.type === 'decrease-quantity'){
+        return{
+            ...state
+        }
+    }
+     if( action.type === 'increase-quantity'){
+        return{
+            ...state
+        }
+    }
+
+}
